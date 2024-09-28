d0c05c7e2fb0845a82d246c623001bb8d814ead4
Girish <girish@post.com>
2024-09-28 12:17:56 -0700

    Integration Tests

---
+++ IntegrationTests/Helpers/TestAuthHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+namespace IntegrationTests.Helpers;
+
+public class TestAuthHandler : AuthenticationHandler<TestAuthenticationSchemeOptions>
+{
+    public TestAuthHandler(
+        IOptionsMonitor<TestAuthenticationSchemeOptions> options, 
+        ILoggerFactory logger, 
+        UrlEncoder encoder,
+        ISystemClock clock) 
+        : base(options, logger, encoder, clock)
+    {
+    }
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var claims = new[]
+        {
+            new Claim("admin", Options.IsAdmin)
+        };
+
+        var identity = new ClaimsIdentity(claims, "Test");
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, "Test");
+
+        var result = AuthenticateResult.Success(ticket);
+
+        return Task.FromResult(result);
+    }
+}
---
+++ IntegrationTests/Helpers/TestAuthenticationSchemeOptions.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace IntegrationTests.Helpers;
+
+public class TestAuthenticationSchemeOptions : AuthenticationSchemeOptions
+{
+    public string IsAdmin { get; set; } = "false";
+}
